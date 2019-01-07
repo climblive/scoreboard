2af57efbfae5b5567e99c09bf93ed9dcb9ea2c56
Jesper Fransson <jesper.fransson@invencon.se>
2019-01-07 21:59:07 +0100

    more redux stuff

--- web/scoreboard/src/index.tsx
+++ web/scoreboard/src/index.tsx
@@ -3,21 +3,20 @@ import * as ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
-/*import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import { StoreState } from './storeState';
+import { IStoreState } from './storeState';
 import { reducer } from './reducers/reducer';
-*/
-/*const store = createStore<StoreState>(reducer, {
+import { Provider } from 'react-redux';
+
+const store = createStore<IStoreState>(reducer, {
    enthusiasmLevel: 1,
    languageName: 'TypeScript',
-});*/
-
-   // <Provider store={store}>
-   // </Provider>
+});
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root') as HTMLElement
+   <Provider store={store}>
+      <App />
+   </Provider>,
+   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();

5c70d5287f9d1182611964d27be5cf77e6425117
Jesper Fransson <jesper.fransson@invencon.se>
2019-01-27 20:12:54 +0100

    Styling of scoreboard. Kind of...

---
+++ web/src/components/ScoreboardListComp.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import './ScoreboardListComp.css';
+import { ScoreboardList } from '../model/scoreboardList';
+
+export interface ScoreboardListCompProps {
+   scoreboardList: ScoreboardList
+}
+
+export function ScoreboardListComp({ scoreboardList }: ScoreboardListCompProps) {
+   var list = scoreboardList.contenders.map(contender =>
+      <div className="contenderRow">
+         <div className="name">{contender.contenderName}</div>
+         <div className="score">{contender.tenBestScore}</div>
+         <div className="score">{contender.totalScore}</div>
+      </div>
+   )
+   
+   return (
+      <div className="scoreboardList">
+         <div className="compClass">{scoreboardList.compClass}</div>
+         {list}
+      </div>
+   );
+}
--- web/src/components/ScoreboardView.tsx
+++ web/src/components/ScoreboardView.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import './ScoreboardView.css';
 import { ScoreboardList } from '../model/scoreboardList';
+import { ScoreboardListComp } from './ScoreboardListComp';
 
 export interface Props {
    scoreboardData: ScoreboardList[];
@@ -12,9 +13,18 @@ export default class ScoreboardView extends React.Component<Props> {
    componentDidMount() {
       this.props.loadScoreboardData!();
    }
+
    render() {
-      return (
-         <div>{JSON.stringify(this.props.scoreboardData)}</div>
-      );
+      var scoreboardData = this.props.scoreboardData;
+      
+      if (scoreboardData) {
+         var list = scoreboardData.map(scoreboardList => <ScoreboardListComp scoreboardList={scoreboardList}/>);
+         return (
+            <div className="scoreboardListContainer">{list}</div>
+         );
+      } else { 
+         return <div>Getting data...</div>
+      }
+
    }
 }

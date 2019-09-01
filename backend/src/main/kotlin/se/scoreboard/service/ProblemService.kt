dda4a8c14b31a1aa3d5b94f619041a6a8541123d
Kasper Ingelvide <kasper@hubben.xyz>
2019-09-01 14:21:52 +0200

    Handle renumbering of problems

--- backend/src/main/kotlin/se/scoreboard/service/ProblemService.kt
+++ backend/src/main/kotlin/se/scoreboard/service/ProblemService.kt
@@ -1,25 +1,107 @@
 package se.scoreboard.service
 
 import org.mapstruct.factory.Mappers
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import se.scoreboard.data.domain.Color
 import se.scoreboard.data.domain.Contest
 import se.scoreboard.data.domain.Problem
+import se.scoreboard.data.repo.ContestRepository
 import se.scoreboard.data.repo.ProblemRepository
 import se.scoreboard.dto.ProblemDto
 import se.scoreboard.mapper.AbstractMapper
 import se.scoreboard.mapper.ProblemMapper
+import javax.transaction.Transactional
 
 @Service
 class ProblemService @Autowired constructor(
-    problemRepository: ProblemRepository) : AbstractDataService<Problem, ProblemDto, Int>(
+    private val problemRepository: ProblemRepository,
+    private val contestRepository: ContestRepository) : AbstractDataService<Problem, ProblemDto, Int>(
         problemRepository) {
 
     override lateinit var entityMapper: AbstractMapper<Problem, ProblemDto>
 
+    companion object {
+        var logger = LoggerFactory.getLogger(ProblemService::class.java)
+    }
+
     init {
         entityMapper = Mappers.getMapper(ProblemMapper::class.java)
+    }
+
+    private enum class ProblemAdjustmentAction {
+        MAKE_ROOM,
+        MOVE_DOWN,
+        CLOSE_GAP
+    }
+
+    @Transactional
+    override fun create(problem: ProblemDto): ProblemDto {
+        handleRenumbering(problem.contestId, ProblemAdjustmentAction.MAKE_ROOM, problem.number)
+        return super.create(problem)
+    }
+
+    @Transactional
+    override fun update(id: Int, problem: ProblemDto): ProblemDto {
+        val old = fetchEntity(id)
+        val oldNumber = old.number
+        val requestedProblemNumber = problem.number
+
+        val action: ProblemAdjustmentAction? = if (requestedProblemNumber == oldNumber) {
+            null
+        } else if (requestedProblemNumber > oldNumber) {
+            ProblemAdjustmentAction.MOVE_DOWN
+        } else {
+            ProblemAdjustmentAction.MAKE_ROOM
+        }
+
+        problem.number = Int.MIN_VALUE
+        super.update(id, problem)
+        problem.number = requestedProblemNumber
+
+        action?.let { handleRenumbering(problem.contestId, it, requestedProblemNumber) }
+
+        return super.update(id, problem)
+    }
+
+    @Transactional
+    override fun delete(id: Int): ProblemDto {
+        val deleted = super.delete(id)
+        entityManager.flush()
+        handleRenumbering(deleted.contestId, ProblemAdjustmentAction.CLOSE_GAP, deleted.number)
+        return deleted
+    }
+
+    private fun handleRenumbering(contestId: Int?, action: ProblemAdjustmentAction, affectedProblemNumber: Int) {
+        val contest = contestRepository.findByIdOrNull(contestId)
+
+        val problems: List<Problem>? = when (action) {
+            ProblemAdjustmentAction.MAKE_ROOM -> contest?.problems?.filter { it.number >= affectedProblemNumber  }
+            ProblemAdjustmentAction.MOVE_DOWN -> contest?.problems?.filter { it.number in 1..affectedProblemNumber }
+            ProblemAdjustmentAction.CLOSE_GAP -> contest?.problems?.filter { it.number > affectedProblemNumber }
+        }?.sortedBy { it.number }
+
+        if (problems.isNullOrEmpty()) {
+            return
+        }
+
+        var startNumber: Int = when (action) {
+            ProblemAdjustmentAction.MAKE_ROOM -> affectedProblemNumber + 1
+            ProblemAdjustmentAction.MOVE_DOWN -> (problems.map { it.number }.min() ?: 0) - 1
+            ProblemAdjustmentAction.CLOSE_GAP -> affectedProblemNumber
+        }
+
+        val intentLog: List<Pair<Int, Problem>>? = problems.map { Pair(startNumber++, it) }
+
+        when (action) {
+            ProblemAdjustmentAction.MAKE_ROOM -> intentLog?.asReversed()
+            ProblemAdjustmentAction.MOVE_DOWN, ProblemAdjustmentAction.CLOSE_GAP -> intentLog
+        }?.forEach {
+            it.second.number = it.first
+            entityManager.flush()
+        }
     }
 
     override fun handleNested(entity: Problem, dto: ProblemDto) {

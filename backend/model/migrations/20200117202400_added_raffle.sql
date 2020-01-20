fa73e40a3d93374468c222c729e8f85cd0747b4a
Kasper Ingelvide <kasper@hubben.xyz>
2020-01-20 17:58:12 +0100

    Migration file fix

--- backend/model/migrations/20200117202400_added_raffle.sql
+++ backend/model/migrations/20200117202400_added_raffle.sql
@@ -9,7 +9,7 @@ CREATE TABLE `raffle` (
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-CREATE INDEX `fk_raffle_1_idx` ON `raffle` (`contest_id` ASC) VISIBLE;
+CREATE INDEX `fk_raffle_1_idx` ON `raffle` (`contest_id` ASC);
 
 CREATE TABLE `raffle_winner` (
   `id` INT NOT NULL AUTO_INCREMENT,
@@ -29,8 +29,8 @@ CREATE TABLE `raffle_winner` (
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-CREATE INDEX `fk_raffle_winner_1_idx` ON `raffle_winner` (`raffle_id` ASC) VISIBLE;
+CREATE INDEX `fk_raffle_winner_1_idx` ON `raffle_winner` (`raffle_id` ASC);
 
-CREATE INDEX `fk_raffle_winner_2_idx` ON `raffle_winner` (`contender_id` ASC) VISIBLE;
+CREATE INDEX `fk_raffle_winner_2_idx` ON `raffle_winner` (`contender_id` ASC);
 
-CREATE UNIQUE INDEX `index4` ON `raffle_winner` (`raffle_id` ASC, `contender_id` ASC) VISIBLE;
+CREATE UNIQUE INDEX `index4` ON `raffle_winner` (`raffle_id` ASC, `contender_id` ASC);

c12b79ce0f1ab3d053abf50c3ada5d9baab2c125
Magnus Osbeck <>
2020-02-06 19:06:26 +0100

    added color to compClasses

--- web/src/components/ScoreboardClassHeaderComp.tsx
+++ web/src/components/ScoreboardClassHeaderComp.tsx
@@ -9,7 +9,7 @@ export interface ScoreboardClassHeaderCompProps {
 export function ScoreboardClassHeaderComp({ compClass }: ScoreboardClassHeaderCompProps) {
    return (
       <div className="compClassHeader">
-         <div className={'compClass-' + compClass.name + " showLarge compClass"}>{compClass.name}</div>
+         <div className={'compClass-' + compClass.scoreboardIndex + " showLarge compClass"}>{compClass.name}</div>
          {!compClass.inProgress && <div className="status">{compClass.statusString}</div>}
          <div className={compClass.inProgress ? 'time active' : 'time'}>{compClass.time}</div>
       </div>

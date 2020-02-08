d05deb7e385fd7e7590e8d62511f248bf43cce31
Kasper Ingelvide <kasper@hubben.xyz>
2020-02-08 15:29:17 +0100

    Verify that comp class does not end before it has started

--- backend/src/main/kotlin/se/scoreboard/service/CompClassService.kt
+++ backend/src/main/kotlin/se/scoreboard/service/CompClassService.kt
@@ -1,10 +1,12 @@
 package se.scoreboard.service
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
 import se.scoreboard.data.domain.CompClass
 import se.scoreboard.data.repo.CompClassRepository
 import se.scoreboard.dto.CompClassDto
+import se.scoreboard.exception.WebException
 import se.scoreboard.mapper.AbstractMapper
 
 @Service
@@ -15,20 +17,24 @@ class CompClassService @Autowired constructor(
 
     override fun onCreate(phase: Phase, new: CompClass) {
         when (phase) {
-            Phase.BEFORE -> clearSeconds(new)
+            Phase.BEFORE -> beforeAnyChange(new)
             else -> {}
         }
     }
 
     override fun onUpdate(phase: Phase, old: CompClass, new: CompClass) {
         when (phase) {
-            Phase.BEFORE -> clearSeconds(new)
+            Phase.BEFORE -> beforeAnyChange(new)
             else -> {}
         }
     }
 
-    private fun clearSeconds(compClass: CompClass) {
+    private fun beforeAnyChange(compClass: CompClass) {
         compClass.timeBegin = compClass.timeBegin?.withSecond(0)?.withNano(0)
         compClass.timeEnd = compClass.timeEnd?.withSecond(0)?.withNano(0)
+
+        if (compClass.timeBegin?.isAfter(compClass.timeEnd) == true) {
+            throw WebException(HttpStatus.BAD_REQUEST, "Cannot end before it has begun")
+        }
     }
 }

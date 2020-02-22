48ada275d2442663877c7f60a3cf5a84c90fc383
Kasper Ingelvide <kasper@hubben.xyz>
2020-02-22 19:58:26 +0100

    Removed /admin/generate and added /version

--- backend/src/main/kotlin/se/scoreboard/api/ApiController.kt
+++ backend/src/main/kotlin/se/scoreboard/api/ApiController.kt
@@ -2,25 +2,23 @@ package se.scoreboard.api
 
 import io.swagger.annotations.Api
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.info.BuildProperties
 import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import se.scoreboard.service.GeneratorService
 import springfox.documentation.annotations.ApiIgnore
 import javax.transaction.Transactional
 
 @RestController
 @CrossOrigin
 @RequestMapping("/api")
-@Api(tags = ["Admin"])
+@Api(tags = ["Api"])
 @ApiIgnore
-class AdminController @Autowired constructor(
-        val generatorService: GeneratorService) {
+class ApiController @Autowired constructor(
+        @Autowired val buildProperties: BuildProperties) {
 
-    @GetMapping("/admin/generate")
+    @GetMapping("/version")
     @Transactional
-    fun generateContest() {
-        generatorService.generateContest()
-    }
+    fun getVersion() = buildProperties
 }

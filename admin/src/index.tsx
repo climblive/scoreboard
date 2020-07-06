ad750fbd118f4790554d3a8e556df99b43a20cd7
Kasper Ingelvide <kasper@hubben.xyz>
2020-07-06 11:01:25 +0200

    Enable Redux DevTools for development

--- admin/src/index.tsx
+++ admin/src/index.tsx
@@ -3,13 +3,23 @@ import * as ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { StoreState } from './model/storeState';
 import { reducer } from './reducers/reducer';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
+import {Environment} from "./environment";
 
-const store = createStore<StoreState, any, any, any>(reducer, {}, applyMiddleware(thunk));
+let middleware = [
+   applyMiddleware(thunk)
+]
+
+if (Environment.siteDomain.startsWith("test")) {
+   const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+   middleware.push((window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__())
+}
+
+const store = createStore<StoreState, any, any, any>(reducer, {}, compose(...middleware));
 
 ReactDOM.render(
    <Provider store={store}>

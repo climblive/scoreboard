6993d7e1453f6680f87af3a8624a16f8b4f2814f
Kasper Ingelvide <kasper@hubben.xyz>
2021-11-21 12:30:52 +0100

    Fix broken Gradle property expansion

--- backend/src/main/kotlin/se/scoreboard/service/PdfService.kt
+++ backend/src/main/kotlin/se/scoreboard/service/PdfService.kt
@@ -112,7 +112,7 @@ class PdfService @Autowired constructor(
         cs.beginText()
         cs.newLineAtOffset(boxX + logoW + m * 3, boxY + m * 3 + 20)
         cs.setFont( PDType1Font.HELVETICA, 10f )
-        cs.showText("Din aktiveringskod på " + webUrl + ":")
+        cs.showText("Din aktiveringskod på " + webUrl)
         cs.endText()
 
         cs.drawImage(logo, boxX + m, boxY + m, logoW, logoH)

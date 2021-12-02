fd67be00927c31e58b04caa1d240c171facea8d0
Kasper Ingelvide <kasper@hubben.xyz>
2021-12-02 19:20:25 +0100

    Replace logout button with avatar to make room for organizer switch

--- admin/src/components/TopMenu.tsx
+++ admin/src/components/TopMenu.tsx
@@ -1,12 +1,11 @@
-import { Button, Grid, Hidden } from "@material-ui/core";
-import Chip from "@material-ui/core/Chip";
+import { Menu } from "@material-ui/core";
+import Avatar from "@material-ui/core/Avatar";
 import FormControl from "@material-ui/core/FormControl";
 import MenuItem from "@material-ui/core/MenuItem";
 import Select from "@material-ui/core/Select";
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
-import AccountCircleIcon from "@material-ui/icons/AccountCircle";
-import ExitToAppIcon from "@material-ui/icons/ExitToApp";
-import React from "react";
+import PersonIcon from "@material-ui/icons/Person";
+import React, { useState } from "react";
 import { connect, ConnectedProps } from "react-redux";
 import { RouteComponentProps, withRouter } from "react-router";
 import { getSelectedOrganizer } from "src/selectors/selector";
@@ -14,7 +13,6 @@ import { logout } from "../actions/actions";
 import { selectOrganizer } from "../actions/asyncActions";
 import { Organizer } from "../model/organizer";
 import { StoreState } from "../model/storeState";
-
 export interface Props {}
 
 const useStyles = makeStyles((theme: Theme) =>
@@ -28,6 +26,13 @@ const useStyles = makeStyles((theme: Theme) =>
     adminChip: {
       marginRight: theme.spacing(1),
     },
+    avatar: {
+      width: theme.spacing(4),
+      height: theme.spacing(4),
+    },
+    menuWrapper: {
+      cursor: "pointer",
+    },
     organizerSelector: { minWidth: 200 },
     selectOption: { color: theme.palette.primary.contrastText },
     loginProgress: { color: "white" },
@@ -36,6 +41,7 @@ const useStyles = makeStyles((theme: Theme) =>
 
 const TopMenu = (props: Props & PropsFromRedux & RouteComponentProps) => {
   const classes = useStyles();
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
 
   const onOrganizerChange = (e: React.ChangeEvent<{ value: unknown }>) => {
     const id = parseInt(e.target.value as string);
@@ -52,72 +58,49 @@ const TopMenu = (props: Props & PropsFromRedux & RouteComponentProps) => {
 
   return (
     <>
-      <Hidden smDown implementation="css">
-        {(props.organizers?.size ?? 0) > 1 && (
-          <FormControl
-            className={classes.organizerSelector}
-            variant="outlined"
-            size="small"
-          >
-            {props.selectedOrganizer !== undefined && (
-              <Select
-                id="series-select"
-                value={props.selectedOrganizer.id}
-                onChange={onOrganizerChange}
-                className={classes.selectOption}
-              >
-                {props.organizers?.toArray()?.map((organizer: Organizer) => (
-                  <MenuItem key={organizer.id} value={organizer.id}>
-                    {organizer.name}
-                  </MenuItem>
-                ))}
-              </Select>
-            )}
-          </FormControl>
-        )}
-      </Hidden>
+      {(props.organizers?.size ?? 0) > 1 && (
+        <FormControl
+          className={classes.organizerSelector}
+          variant="outlined"
+          size="small"
+        >
+          {props.selectedOrganizer !== undefined && (
+            <Select
+              id="series-select"
+              value={props.selectedOrganizer.id}
+              onChange={onOrganizerChange}
+              className={classes.selectOption}
+            >
+              {props.organizers?.toArray()?.map((organizer: Organizer) => (
+                <MenuItem key={organizer.id} value={organizer.id}>
+                  {organizer.name}
+                </MenuItem>
+              ))}
+            </Select>
+          )}
+        </FormControl>
+      )}
 
       <div className={classes.authControl}>
         {props.loggedInUser && (
-          <div>
-            <Grid container direction="row" alignItems="center">
-              <Grid item>
-                <div>
-                  <Grid
-                    container
-                    direction="row"
-                    alignItems="flex-end"
-                    wrap="wrap"
-                  >
-                    <Grid item>
-                      <div className={classes.userName}>
-                        {props.loggedInUser.name}
-                      </div>
-                    </Grid>
-                    {props.loggedInUser.admin && (
-                      <Hidden xsDown>
-                        <Grid item>
-                          <div className={classes.adminChip}>
-                            <Chip
-                              icon={<AccountCircleIcon />}
-                              label="Admin"
-                              color="secondary"
-                              size="small"
-                            />
-                          </div>
-                        </Grid>
-                      </Hidden>
-                    )}
-                  </Grid>
-                </div>
-              </Grid>
-              <Grid item>
-                <Button variant="contained" onClick={logout} size="small">
-                  <ExitToAppIcon /> Logout
-                </Button>
-              </Grid>
-            </Grid>
-          </div>
+          <>
+            <div
+              className={classes.menuWrapper}
+              onClick={(event) => setAnchorEl(event.currentTarget)}
+            >
+              <Avatar className={classes.avatar}>
+                <PersonIcon />
+              </Avatar>
+            </div>
+            <Menu
+              anchorEl={anchorEl}
+              keepMounted
+              open={Boolean(anchorEl)}
+              onClose={() => setAnchorEl(null)}
+            >
+              <MenuItem onClick={logout}>Logout</MenuItem>
+            </Menu>
+          </>
         )}
       </div>
     </>

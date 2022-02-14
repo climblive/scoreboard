cf3cd2bf024a6a8079b6a9452c71355be55160a7
Kasper Ingelvide <kasper@hubben.xyz>
2022-02-14 21:01:18 +0100

    Fix

--- admin/src/environment.tsx
+++ admin/src/environment.tsx
@@ -1,4 +1,4 @@
 export class Environment {
   public static siteDomain = "test.climblive.app";
-  public static projectVersion = "1.1-280-g1534460.dirty";
+  public static projectVersion = "0.0";
 }

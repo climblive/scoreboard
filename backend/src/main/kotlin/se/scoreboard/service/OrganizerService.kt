5d59ccf891ff67b372e65ccf7ab9173e257c8b4d
Kasper Ingelvide <kasper@hubben.xyz>
2023-12-30 18:14:29 +0100

    Let admin user retrieve a list of all organizers

--- backend/src/main/kotlin/se/scoreboard/service/OrganizerService.kt
+++ backend/src/main/kotlin/se/scoreboard/service/OrganizerService.kt
@@ -8,6 +8,7 @@ import se.scoreboard.data.domain.Organizer
 import se.scoreboard.data.domain.User
 import se.scoreboard.data.repo.OrganizerRepository
 import se.scoreboard.data.repo.UserRepository
+import se.scoreboard.dto.ColorDto
 import se.scoreboard.dto.OrganizerDto
 import se.scoreboard.getUserPrincipal
 import se.scoreboard.mapper.AbstractMapper

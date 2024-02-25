43690df400a36ea63782bdbb60a5809d479cff9b
Kasper Ingelvide <7457770+kasper-i@users.noreply.github.com>
2024-02-25 19:46:53 +0100

    Provide name of raffle winner from backend (#337)

--- backend/src/main/kotlin/se/scoreboard/dto/RaffleWinnerDto.kt
+++ backend/src/main/kotlin/se/scoreboard/dto/RaffleWinnerDto.kt
@@ -6,7 +6,8 @@ data class RaffleWinnerDto (
     var id: Int?,
     var raffleId: Int,
     var contenderId: Int,
+    var contenderName: String,
     var timestamp: OffsetDateTime) {
 
-    constructor() : this(null, Int.MIN_VALUE, Int.MIN_VALUE, OffsetDateTime.MIN)
+    constructor() : this(null, Int.MIN_VALUE, Int.MIN_VALUE, "", OffsetDateTime.MIN)
 }
